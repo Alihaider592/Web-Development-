f0196abbcb3a3c4d10d5da603fa9e4d5318050de
MalikAli007884 <malibro007800@gmail.com>
2025-07-31 23:21:10 -0700

    variables  using website

---
+++ java script variables/script.js
@@ -0,0 +1,21 @@
+alert("Welcome To Netflix")
+
+let = a = 51
+console.log(a)
+let = b = "Ali haider"
+console.log(b)
+let = c = null
+console.log(c)
+
+console.log( typeof a, typeof b, typeof c)
+let = d = 73
+console.log(a + d)
+
+prompt  ("Enter your Number")
+console.log(prompt)
+prompt ("Enter your password")
+console.log(prompt)
+confirm("Your account has successfully loging in")
+console.log(confirm)
+
+
