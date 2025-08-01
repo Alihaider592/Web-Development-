a8213d4b115e02c6f86814d5ed158ba7ebce6a7c
MalikAli007884 <malibro007800@gmail.com>
2025-07-31 23:25:40 -0700

    driving age checker

---
+++ java script DOM/script.js
@@ -0,0 +1,19 @@
+let age = prompt("enter your age")  
+
+ if (age<0) {
+    console.log("error of age")
+    prompt("please enter a valid age")
+}
+else{
+    if (age>=18) {
+        alert("you can drive")
+        console.log(`${a}`)
+        let age = number(age)
+    }
+    else{
+        alert("you can't drive")
+        // console.log(`${a}`)
+    }
+confirm("you can enter your age again")
+}
+
