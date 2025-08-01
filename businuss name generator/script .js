c2b4d5f27ca52dacd40fac31353a80473d0778a7
MalikAli007884 <alicoder592@gmail.com>
2025-07-31 23:30:34 -0700

    A business name generator

---
+++ businuss name generator/script .js
@@ -0,0 +1,34 @@
+// let rendom = math .rendom()
+// if (rendom<0.33) {
+//     first = "crazy"
+// }
+// else if (rendom < 0.66 &&  rendom>= 0.33) {
+//     second = "amaizing"
+// }
+// else{
+//     second = "fire"
+// }
+// // let rendom = math .rendom()
+// if (rendom<0.33) {
+//     second = "engine"
+// }
+// else if (rendom < 0.66 &&  rendom>= 0.33) {
+//     second = "food"
+// }
+// else{
+//     second = "garments"
+// }
+
+// // let rendom = math .rendom()
+
+// if (rendom<0.33) {
+//     third = "bros"
+// }
+// else if (rendom < 0.99 &&  rendom>= 0.66) {
+//     third = "limited"
+// }
+// else{
+//     third = "hub"
+// }
+
+// console.log(`${first} ${second} ${third}`)
