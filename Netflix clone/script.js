45afee99450e961c3f29a67217802fdd7bcbe980
MalikAli007884 <malibro007800@gmail.com>
2025-07-31 22:50:04 -0700

    netflix clone by using HTML and CSS

---
+++ Netflix clone/script.js
@@ -0,0 +1,16 @@
+alert("Welcom To Netflix")
+
+let = a = 51
+console.log(a)
+let = b = "Ali haider"
+console.log(b)
+let = c = null
+console.log(c)
+
+console.log( typeof a, typeof b, typeof c)
+let = d = 73
+console.log(a + d)
+
+prompt("Enter your Number")
+prompt("Enter your password")
+confirm("Your account has successfully loging in")
